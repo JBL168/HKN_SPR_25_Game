ff355e71502ea34e8ec55cb9a9ca1ffd5c841ead
Josh200315 <50426132+Josh200315@users.noreply.github.com>
2025-03-25 17:05:39 -0500

    Preliminary collider implementations

--- src/main.py
+++ src/main.py
@@ -3,11 +3,12 @@ import sys
 import pygame
 from environment import Environment
 from player import Player
+from physics import BoxCollider, CircleCollider
 
 WIDTH, HEIGHT = 1280, 720
 FPS = 60
 
-def main(window):
+def main():
     '''
     Stub for initializing the player.
     
@@ -23,8 +24,16 @@ def main(window):
     window = pygame.display.set_mode((WIDTH, HEIGHT))
     clock = pygame.time.Clock()
 
+    box1 = BoxCollider(pygame.Vector2(10, 10), pygame.Vector2(5, 5))
+    circle1 = CircleCollider(5, pygame.Vector2(10, 14.9))
+
+    print(box1.checkCollision(circle1).direction)
+
     running = True
     while running:
+        window.fill("white")
+
+        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
--- src/physics.py
+++ src/physics.py
@@ -1,6 +1,9 @@
+from typing import Self
 import pygame
+from pygame import Vector2
+from abc import ABC, abstractmethod
 
-class Physics:
+class PhysicsObject:
   def __init__(self, position=(0,0), size=(50,50)):
       '''
       Stub for initializing an object. These attributes will be inherited by player.
@@ -49,3 +52,101 @@ class Physics:
       a list of collisions (empty if no collisions are detected).
     '''
     pass
+
+'''
+An abstract Collider class defining a standard interface for manipulating colliders of all shapes.
+Attempting to instantiate this class is illegal.
+'''
+class Collider(ABC):
+  '''
+  An object containing information about a collision between two Colliders.
+  '''
+  class Collision:
+    def __init__(self, normal: Vector2):
+      self.direction = normal
+
+    def invertPerspective(self) -> Self:
+      self.direction = -self.direction
+      return self
+  
+  def __init__(self, center: Vector2 = (0, 0)):
+    self._position = center
+
+  def moveTo(self, position: Vector2) -> None:
+    self._position = position
+
+  @property
+  @abstractmethod
+  def boundingBox(self) -> tuple[Vector2, Vector2]:
+    pass
+
+  def checkCollision(self, other: Self) -> None | Collision:
+    (x1, y1), (x2, y2) = self.boundingBox
+    (ox1, oy1), (ox2, oy2) = other.boundingBox
+    if (x2 < ox1 or y2 < oy1 or x1 > ox2 or y1 > oy2):
+      return None
+    return self._checkCollision(other)
+
+  @abstractmethod
+  def _checkCollision(self, other: Self) -> None | Collision:
+    pass
+
+'''
+A simple axis-aligned box collider that is not allowed to rotate.
+'''
+class BoxCollider(Collider):
+  def __init__(self, size: Vector2, center: Vector2 = (0, 0)):
+    super().__init__(center)
+    self._size = size
+
+  @property
+  def boundingBox(self) -> tuple[Vector2, Vector2]:
+    return self._position - self._size/2, self._position + self._size/2
+  
+  def _checkCollision(self, other: Collider) -> None | Collider.Collision:
+    if isinstance(other, BoxCollider):
+      (x1, y1), (x2, y2) = self.boundingBox       # Upper-left and lower-right corner coordinates of this box
+      (ox1, oy1), (ox2, oy2) = other.boundingBox  # Same for other box
+      xc, yc = self._position                     # Center coordinates of this box
+      oxc, oyc = other._position                  # Same for other box
+      overlapX = min(x2, ox2) - max(x1, ox1)      # overlapX and overlapY are the dimensions of the space enclosed by both boxes
+      overlapY = min(y2, oy2) - max(y1, oy1)
+      if oxc > xc:    # Right side collision
+        if oyc < yc:  # Other box's center is above this box's center
+          return Collider.Collision(Vector2((1, 0) if overlapX < overlapY else (0, -1)))
+        else:
+          return Collider.Collision(Vector2((1, 0) if overlapX < overlapY else (0, 1)))
+      else:           # Left side collision
+        if oyc < yc:
+          return Collider.Collision(Vector2((-1, 0) if overlapX < overlapY else (0, -1)))
+        else:
+          return Collider.Collision(Vector2((-1, 0) if overlapX < overlapY else (0, 1)))
+
+    if collision := other._checkCollision(self):
+      return collision.invertPerspective()
+
+'''
+A circular collider. It implicitly supports rotation because its shape does not change when it is rotated.
+'''
+class CircleCollider(Collider):
+  def __init__(self, radius: float, center: Vector2 = (0, 0)):
+    super().__init__(center)
+    self._radius = radius
+  
+  @property
+  def boundingBox(self) -> tuple[Vector2, Vector2]:
+    return self._position.elementwise() - self._radius, self._position.elementwise() + self._radius
+  
+  def _checkCollision(self, other: Collider) -> None | Collider.Collision:
+    if isinstance(other, CircleCollider):
+      positionDifference = self._position - other._position
+      if positionDifference.length() < self._radius + other._radius:
+        return Collider.Collision(positionDifference.normalize().elementwise() * Vector2(1, -1))
+      return None
+    elif isinstance(other, BoxCollider):
+      (ox1, oy1), (ox2, oy2) = other.boundingBox
+      for corner in ((ox1, oy1), (ox1, oy2), (ox2, oy1), (ox2, oy2)):
+        if (direction := self._position - Vector2(*corner)).length() < self._radius:
+          return Collider.Collision(direction.normalize().elementwise() * Vector2(1, -1))
+      else:
+        return BoxCollider(Vector2(self._radius, self._radius) * 2, self._position)._checkCollision(other)
